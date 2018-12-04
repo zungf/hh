f4521b48bbe49b8a11c5a641f08117e85703cbf0
Zino <41270843+zungf@users.noreply.github.com>
2018-12-04 21:16:25 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,13 @@
 const Discord = require("discord.js");
-const ytdl = require("ytdl-core");
-const { Client, Util } = require('discord.js');
-
- 
-
+const client = new Discord.Client();
 var prefix = "Z";
-
-
 client.on("message", message => {
+
             if (message.content.startsWith(prefix + "bc")) {
                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
   let args = message.content.split(" ").slice(1);
   var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'online').forEach(m => {
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
  m.send(`${argresult}\n ${m}`);
 })
  message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
@@ -28,21 +23,14 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Zino`,"http://twitch.tv/S-F")
+client.user.setGame(`$bc | OG,SERVER`,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 
 
 
 
-
-
-
-
-
-
-
-
+c
 
 
 
